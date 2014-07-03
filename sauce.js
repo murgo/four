64522143cf49277f10f1763fcab9c2e00bca3f7b
Lauri Härsilä <murgo@iki.fi>
2014-07-03 03:54:36 +0300

    Maybe it works now

--- sauce.js
+++ sauce.js
@@ -24,11 +24,29 @@
     function init() {
         var container = document.getElementById('canvas-container');
 
-        // some example engine options
+        var w = $(window).width() - 25;
+        var h = $(window).height() - 25;
+
         var options = {
             positionIterations: 6,
             velocityIterations: 4,
-            enableSleeping: false
+            enableSleeping: false,
+            world: {
+                bounds: {
+                    min: {x: 0, y: 0},
+                    max: {x: w, y: h}
+                }
+            },
+            render: {
+                options: {
+                    width: w,
+                    height: h
+                },
+                bounds: {
+                    min: {x: 0, y: 0},
+                    max: {x: w, y: h}
+                }
+            }
         };
 
         // create a Matter engine
@@ -43,6 +61,7 @@
         Engine.run(_engine);
 
         reset();
+        pipe();
 
         var renderOptions = _engine.render.options;
         renderOptions.wireframes = false;
@@ -58,6 +77,35 @@
         window.addEventListener('load', init);
     } else if (window.attachEvent) {
         window.attachEvent('load', init);
+    }
+    
+    function pipe() {
+        var w = $(window).width() - 25;
+        var h = $(window).height() - 25;
+        var pipe = $("#pipe");
+        var left = (w - pipe.width()) / 2;
+        var top = h - pipe.height();
+        pipe.css({top: top, left: left});
+
+        var inputDiv = $("#board-div");
+        inputDiv.css({top: top + 150, left: (w - inputDiv.width()) / 2 });
+        var input = $("#input-board");
+        input.val(board);
+        input.keyup(function(event){
+            if(event.keyCode == 13){
+                window.location.href = "?board=" + input.val();
+            }
+        });        
+
+        var pipewidth1 = 235;
+        var pipeheight1 = 89;
+        var pipewidth2 = 181;
+        var pipeheight2 = 213;
+        var offset = 8;
+        World.add(_engine.world, [
+            Bodies.rectangle(left + pipe.width() / 2 - offset, top + pipeheight1 / 2 - offset, pipewidth1, pipeheight1, { isStatic: true }),
+            Bodies.rectangle(left + pipe.width() / 2 - offset, top + pipeheight1 + pipeheight2 / 2 - offset, pipewidth2, pipeheight2, { isStatic: true }),
+        ]);
     }
 
     function reset() {
@@ -84,11 +132,16 @@
         _engine.timing.timeScale = 1;
 
         var offset = 5;
+        var x2 = _engine.render.options.width;
+        var y2 = _engine.render.options.height;
+        var x1 = x2 / 2;
+        var y1 = y2 / 2;
+        var asd = 50.5;
         World.add(_world, [
-            Bodies.rectangle(400, -offset, 800.5 + 2 * offset, 50.5, { isStatic: true }),
-            Bodies.rectangle(400, 600 + offset, 800.5 + 2 * offset, 50.5, { isStatic: true }),
-            Bodies.rectangle(800 + offset, 300, 50.5, 600.5 + 2 * offset, { isStatic: true }),
-            Bodies.rectangle(-offset, 300, 50.5, 600.5 + 2 * offset, { isStatic: true })
+            Bodies.rectangle(x1, -offset, x2 + 2 * offset, asd, { isStatic: true }),
+            Bodies.rectangle(x1, y2 + offset, x2 + 2 * offset, asd, { isStatic: true }),
+            Bodies.rectangle(x2 + offset, y1, asd, y2 + 2 * offset, { isStatic: true }),
+            Bodies.rectangle(-offset, y1, asd, y2 + 2 * offset, { isStatic: true })
         ]);
 
         _mouseConstraint = MouseConstraint.create(_engine);
@@ -113,23 +166,33 @@
             renderOptions.showDebug = true;
     };
 
+    function createBody(url, width, height) {
+        var sprite = { texture: url };
+        var w = Common.random(50, 200);
+        var ratio = w / width;
+        var h = ratio * height;
+        sprite.xScale = ratio;
+        sprite.yScale = ratio;
+        
+        var x = _engine.render.options.width / 2;
+        var y = _engine.render.options.height / 2;
+
+        var body = Bodies.rectangle(x, y, w, h, { angle: Common.random(0, 2*Math.PI), torque: Common.random(0, 150) * ratio, render: { sprite: sprite } });
+        Body.applyForce(body, { x: x, y: y } , { x: Common.random(-2, 2) * ratio, y: Common.random(-2, 0) * ratio });
+        return body;
+    }
+    
     function imageLoaded(url, width, height) {
         var _world = _engine.world;
 
+        /*
         var stack = Composites.stack(20, 20, 15, 4, 0, 0, function(x, y, column, row) {
-            // round the edges of some bodies
-            
-            var sprite = { texture: url };
-            var w = Common.random(10, 200);
-            var ratio = w / width;
-            var h = ratio * height;
-            sprite.xScale = ratio;
-            sprite.yScale = ratio;
-
-            return Bodies.rectangle(x, y, w, h, { angle: Common.random(0, 2*Math.PI), render: { sprite: sprite } });
+            return createBody(url, x, y, width, height);
         });
 
         World.add(_world, stack);
+        */
+        World.add(_world, createBody(url, width, height));
     };
     
     var board = 'b';
@@ -142,7 +205,8 @@
     function parseJson(js) {
         if (typeof js == "object") {
             if (js.ext !== undefined && js.tim !== undefined) {
-                parsed.push(js.tim + js.ext)
+                //parsed.push(js.tim + js.ext);
+                parsed.push(js.tim + "s.jpg"); // thumbnail
             }
         
             $.each(js, function(k,v) {
@@ -153,22 +217,24 @@
     }
     
     function loadImages() {
-    /*
-        $.getJson("json", {board: board}, function(data) {
+        $.getJSON("json/" + board + "/catalog.json", null, function(data) {
             parseJson(data);
             loadImage(0);
         });
-        */
     };
     
     function loadImage(i) {
         var img = new Image();
         img.addEventListener('load', function() {
             imageLoaded(img.src, img.naturalWidth, img.naturalHeight);
-            setTimeout(function() { loadImage(i + 1}, 1000);
+            setTimeout(function() { loadImage(i + 1); }, 1000);
         }, false);
-        img.src = 'http://narf-archive.com/pix/ca639b9e18fda05a6cc2eb1eed668c212c41d0ab.gif';
-        //img.src = "http://i.4cdn.org/" + board + "/" + parsed[i];
+        img.addEventListener('error', function() {
+            setTimeout(function() { loadImage(i + 1); }, 1000);
+        }, false);
+        img.addEventListener('click', function() { console.log('clicked' + parsed[i] )}, false);
+        //img.src = "http://t.4cdn.org/" + board + "/" + parsed[i];
+        img.src = "t/" + board + "/" + parsed[i];
     }
 
     function getSearchParameters() {
